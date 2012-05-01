314b18f11416bea1c285b3e3afdf75d586b38e27
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-01 14:07:26 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule
    git-svn-id: http://svn.foswiki.org/trunk/GenPDFLatexAddOn@14680 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Contrib/GenPDFLatexAddOn/build.pl
+++ lib/Foswiki/Contrib/GenPDFLatexAddOn/build.pl
@@ -3,14 +3,14 @@
 # Build for GenPDFLatexAddOn
 #
 BEGIN {
-    unshift @INC, split(/:/, $ENV{FOSWIKI_LIBS});
+    unshift @INC, split( /:/, $ENV{FOSWIKI_LIBS} );
 }
 
 use Foswiki::Contrib::Build;
 
 # Create the build object
-$build = new Foswiki::Contrib::Build( 'GenPDFLatexAddOn' );
+$build = new Foswiki::Contrib::Build('GenPDFLatexAddOn');
 
 # Build the target on the command line, or the default target
-$build->build($build->{target});
+$build->build( $build->{target} );
 
